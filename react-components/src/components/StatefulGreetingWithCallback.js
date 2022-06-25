794e5e3ed9ab49b181f68775346fdd8184272f5d
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-06-25 13:24:58 +0000

    Add callback function to update state effectively in console

---
+++ react-components/src/components/StatefulGreetingWithCallback.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+class StatefulGreetingWithCallback extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      introduction: "Hello!",
+      buttonText: "Exit",
+    };
+
+  }
+
+  handleClick() {
+    this.setState({
+      introduction: "Goodbye!",
+      buttonText: "Enter",
+    }, ()=>{
+        console.log('new state', this.state.introduction);
+        console.log('new state', this.state.buttonText);
+    }); // callback functionconsole.log(this.state.introduction);
+    console.log(this.state.buttonText);
+    console.log(this.state.introduction);
+    console.log(this.state.buttonText);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.introduction}, {this.props.greeting}</h1>
+        <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+      </div>
+    ) 
+  }
+}
+
+export default StatefulGreetingWithCallback
+
+
+// export default class StatefulGreeting extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+//   }
+
+//   setCount = () => {
+//     this.setState(prevState => {
+//       return { count: prevState.count + 1 }
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="greeting">
+//         <h1>
+//           Hello, {this.props.name}, I'm a stateful component!
+//         </h1>
+//         <h2>You've clicked {this.state.count} times</h2>
+//         <button onClick={this.setCount}>Increment Count</button>
+//       </div>
+//     );
+//   }
+// }
